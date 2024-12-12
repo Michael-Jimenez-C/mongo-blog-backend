f3a310340f91f451d48fccc442fb218744bc04ca
Michael Jimenez <michaeljimenezcontreras1@gmail.com>
2024-12-11 23:22:12 -0500

    Pequeña descripción al endpoint

--- src/routers/admin.py
+++ src/routers/admin.py
@@ -2,11 +2,15 @@ from fastapi import APIRouter
 import os
 
 router = APIRouter(
-    tags=["admin"]
+    tags=["Admin"]
 )
 
 @router.post('/configure')
 async def configure(passwd: str):
+    """
+    Utility endpoint to configure the database and create the necessary indexes.
+    ADMIN_PSSWD must be set in the environment variables.
+    """
     if os.environ.get('ADMIN_PSSWD') == passwd:
         from engine import Engine
         from models import DBMODEL
